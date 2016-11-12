e7aae2682ed1709005055fa81d8c31a8e714c7d6
making <makingx@gmail.com>
2016-11-13 00:59:49 +0900

    remoce source

--- src/main/java/com/example/AggregateFilterAndTransformApplication.java
+++ src/main/java/com/example/AggregateFilterAndTransformApplication.java
@@ -3,7 +3,6 @@ package com.example;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.aggregate.AggregateApplicationBuilder;
 import org.springframework.cloud.stream.app.filter.processor.rabbit.FilterProcessorRabbitApplication;
-import org.springframework.cloud.stream.app.http.source.rabbit.HttpSourceRabbitApplication;
 import org.springframework.cloud.stream.app.transform.processor.rabbit.TransformProcessorRabbitApplication;
 
 @SpringBootApplication
@@ -11,8 +10,7 @@ public class AggregateFilterAndTransformApplication {
 
 	public static void main(String[] args) {
 		new AggregateApplicationBuilder()
-				.from(HttpSourceRabbitApplication.class)
-				.via(FilterProcessorRabbitApplication.class)
+				.from(FilterProcessorRabbitApplication.class)
 				.args("--expression=#jsonPath(payload,'$.lang').equals('ja')")
 				.to(TransformProcessorRabbitApplication.class)
 				.args("--expression=payload.user.screen_name + ' ' + payload.text")
